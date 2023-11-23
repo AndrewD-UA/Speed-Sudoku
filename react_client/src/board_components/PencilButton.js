3e0c7256e005d5fea1c262af85aaeaa09b5a2a3f
CatharticSalt <andrewdennison87@gmail.com>
2023-11-22 17:03:58 -0700

    Adding same pressing functionality to the Pencil button

--- react_client/src/board_components/PencilButton.js
+++ react_client/src/board_components/PencilButton.js
@@ -5,20 +5,53 @@ export class PencilButton extends Component{
         super();
 
         this.state = {
-            eraseMode : false
+            pencilMode : false
         };
 
         this.parentBoard = props.board;
-        this.handleClick = this.handleClick.bind(this);
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.handleMouseOut = this.handleMouseOut.bind(this);
+        this.handleMouseUp = this.handleMouseUp.bind(this);
     }
 
-    handleClick(){
+    togglePencilMode(){
+        if (this.state.pencilMode === false){
+            this.setState({
+                pencilMode: true
+            });
+        } else {
+            this.setState({
+                pencilMode: false
+            });
+        }
+    }
+
+    handleMouseDown(){
+        this.held = true;
+        this.togglePencilMode();
         console.log("clicked");
+    }
+
+    handleMouseOut(){
+        if (this.state.pencilMode === true && this.held === true){
+            this.togglePencilMode();
+        }
+
+        this.held = false;
+    }
+
+    handleMouseUp(){
+        this.held = false;
     }
 
     render(){
         return (
-            <input className="optionButton" type="button" value="Pencil"/>
+            <input  className={this.state.pencilMode ? "toggledOptionButton" : "optionButton"} 
+                    type="button" value="Pencil"
+                    onClick={this.handleClick}
+                    onMouseDown={this.handleMouseDown}
+                    onMouseOut={this.handleMouseOut}
+                    onMouseUp={this.handleMouseUp}/>
         )
     }
 }
